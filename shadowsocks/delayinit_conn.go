de6fd3e0c571309787ca9ff9c6564f3216cf7213
shinku721 <shinku721@protonmail.com>
2017-07-20 07:50:25 -0400

    fix delayinit

--- shadowsocks/delayinit_conn.go
+++ shadowsocks/delayinit_conn.go
@@ -1,45 +1,42 @@
 package shadowsocks
 
 type DelayInitConn struct {
-  origConn SSConn
-  initBuf []byte
+	origConn SSConn
+	initBuf  []byte
 }
 
 func (c *DelayInitConn) SSRead(buf *SSBuffer) error {
-  return c.origConn.SSRead(buf)
+	return c.origConn.SSRead(buf)
 }
 
 func (c *DelayInitConn) SSWrite(buf *SSBuffer) error {
-  if c.initBuf != nil {
-    li := len(c.initBuf)
-    lb := len(buf.buf)
-    if li + lb > cap(buf.buf) {
-      if err := buf.Expand(li + lb); err != nil {
-        return err
-      }
-    }
-    copy(buf.buf[li:li+lb], buf.buf[:lb])
-    copy(buf.buf[:li], c.initBuf)
-    c.initBuf = nil
-  }
-  return c.origConn.SSWrite(buf)
+	if c.initBuf != nil {
+		li := len(c.initBuf)
+		lb := len(buf.buf)
+		tmpbuf := make([]byte, li+lb)
+		copy(tmpbuf[:li], c.initBuf)
+		copy(tmpbuf[li:], buf.buf)
+		c.initBuf = nil
+		return c.origConn.SSWrite(&SSBuffer{buf: tmpbuf})
+	}
+	return c.origConn.SSWrite(buf)
 }
 
 func (c *DelayInitConn) Close() error {
-  return c.origConn.Close()
+	return c.origConn.Close()
 }
 
 func (c *DelayInitConn) Alive() bool {
-  return c.origConn.Alive()
+	return c.origConn.Alive()
 }
 
 func (c *DelayInitConn) RemoteAddr() string {
-  return c.origConn.RemoteAddr()
+	return c.origConn.RemoteAddr()
 }
 
-func NewDelayInitConn(conn SSConn, initBuf []byte) (*DelayInitConn) {
-  return &DelayInitConn{
-    origConn: conn,
-    initBuf: initBuf,
-  }
+func NewDelayInitConn(conn SSConn, initBuf []byte) *DelayInitConn {
+	return &DelayInitConn{
+		origConn: conn,
+		initBuf:  initBuf,
+	}
 }
